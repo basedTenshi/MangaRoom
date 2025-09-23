abdc393251a2b6082cf211097b8f62229fa2d7c3
Tenshi <158788527+basedTenshi@users.noreply.github.com>
2025-09-23 19:11:36 +0800

    AnimeHub React Prototype 0.1.2

---
+++ AnimeHub-app/src/header.jsx
@@ -0,0 +1,18 @@
+const topHeader = (<div className="top-header">
+    <div className="header-logo">
+        <img src="AnimeHub-LogoText.png" alt="AnimeHub Logo"/>
+    </div>
+    <div className="header-title">
+        <h1>AnimeHub</h1>
+    </div>
+    <div className="header-navigation">
+        <a href = "#home">Home</a>
+        <a href = "#ongoing">Ongoing</a>
+        <a href = "#upcoming">Upcoming</a>
+        <a href = "#about">Community</a>
+        <a href = "#contact">Contact</a>
+    </div>
+    <div className="header-search">
+        <input type="text" placeholder="Search"/>
+    </div>
+</div>);
---
+++ AnimeHub-app/src/landingpage.jsx
@@ -0,0 +1,16 @@
+import topHeader from './header.jsx';
+
+const landingPageHeader = topHeader;
+const landingPageText =
+    (<div className="header-text">
+    <h3>
+        Welcome to AnimeHub, the ultimate destination for anime lovers!
+        <br/>
+        <br/>
+        Discover the latest ongoing series and find out where to watch them for free.
+        <br/>
+        <br/>
+        Our platform features a user-rating system so you can share your opinions and a vibrant discussion board to
+        connect with a passionate community of fans.
+    </h3>
+</div>);
