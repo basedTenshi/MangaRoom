786a173190ffe3f479a3c83d273c6ee967c070a8
Tenshi <158788527+basedTenshi@users.noreply.github.com>
2025-11-13 15:30:09 +0800

    MangaHub React Prototype 1.3
    - Updated fetch logic and data manipulation logic to render in React page

--- mangaroom-backend/src/server.js
+++ mangaroom-backend/src/server.js
@@ -1,13 +1,17 @@
 import express from 'express';
+import cors from 'cors';
 import startDatabase from './database.js';
 
 const app = express();
 const port = 3000;
 
-// req.params, get whats being requested such as "user_id" then pass it as argument to the startDatabase async function and retrieve the promise data
+app.use(cors());
+app.use(express.json());
+
+// req.params, get what's being requested such as "user_id" then pass it as argument to the startDatabase async function and retrieve the promise data
 // through then()
 app.get('/', (req, res) => {
-    const database = startDatabase(req.params).then(data => {res.json(data)});
+    const totalMangaTrackedResponse = startDatabase('manga_count').then(data => {res.send(data)});
 })
 
 app.listen(port, () => {
